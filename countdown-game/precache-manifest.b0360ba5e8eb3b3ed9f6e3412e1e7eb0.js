a55c190b8c1a2b6d2dcbc40b8409e7a8a6edca0c
Mostafa Mabrouk <mostafa.mabrouk@code125.com>
2019-07-13 20:04:19 +0200

    1.0.0

---
+++ countdown-game/precache-manifest.b0360ba5e8eb3b3ed9f6e3412e1e7eb0.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30ee7df7c9b1ff0f3f5e9012905197eb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "047e31c657aad10ef1cc",
+    "url": "/static/js/2.8ee0a0ac.chunk.js"
+  },
+  {
+    "revision": "b6f24c7b2bbe5d392ebf",
+    "url": "/static/js/main.4b58fe65.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
